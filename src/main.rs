0aaed816d291aff7d50b672940755eb18f177a87
Bart van Pelt <bart.vanpelt@gmail.com>
2026-07-15 13:32:25 +0200

    echoserver

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
-extern crate term;
+use std::io::{Error, Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
+    println!("Incoming connection from: {}", stream.peer_addr()?);
+    let mut buf = [0; 512];
+
+    loop {
+        let bytes_read = stream.read(&mut buf)?;
+        if bytes_read == 0 {
+            return Ok(());
+        }
+        stream.write(&buf[..bytes_read])?;
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
-
-    let mut t = term::stdout().unwrap();
-    t.fg(term::color::GREEN).unwrap();
-    write!(t, "Hello, ").unwrap();
-    t.fg(term::color::RED).unwrap();
-    writeln!(t, "world!").unwrap();
-    t.reset().unwrap();
+    let listener = TcpListener::bind("0.0.0.0:8888").expect("Could not bind");
+    for stream in listener.incoming() {
+        match stream {
+            Err(e) => {
+                eprintln!("failed: {}", e)
+            }
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_client(stream).unwrap_or_else(|error| eprintln!("{:?}", error));
+                });
+            }
+        }
+    }
 }
